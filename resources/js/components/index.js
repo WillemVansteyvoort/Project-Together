909730a7c2dbf26180f0a3f2967902cbb8a11e47
WillemVansteyvoort <willemvansteyvoort@outlook.com>
2018-11-29 17:15:31 +0100

    Verwijder alles

--- resources/js/components/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import SignUpStep1 from './signup/step1'
-
-
-if (document.getElementById('root')) {
-    ReactDOM.render(<SignUpStep1 />, document.getElementById('root'));
-}
